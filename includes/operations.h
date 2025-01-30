78287c683eb4158ce2e43f88e22949a3c2b1f974
Claudine Meryl Metee Bitee <cmetee-b@bess-f3r3s11.clusters.42paris.fr>
2025-01-30 17:56:49 +0100

    debut algo

--- includes/operations.h
+++ includes/operations.h
@@ -6,7 +6,7 @@
 /*   By: cmetee-b <cmetee-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 10:42:35 by cmetee-b          #+#    #+#             */
-/*   Updated: 2025/01/30 11:21:41 by cmetee-b         ###   ########.fr       */
+/*   Updated: 2025/01/30 17:44:25 by cmetee-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,35 +19,35 @@
 ** Échange les 2 premiers éléments au sommet de la pile
 ** Ne fait rien s'il n'y en a qu'un ou aucun
 */
-void	swap_a(t_stack *a);
-void	swap_b(t_stack *b);
-void	swap_s(t_stack *a, t_stack *b);
+void	swap_a(t_stack *a, int print);
+void	swap_b(t_stack *b, int print);
+void	swap_s(t_stack *a, t_stack *b, int print);
 
 /*
 ** --- PUSH OPERATIONS ---
 ** Prend le premier élément au sommet d'une pile et le met sur l'autre pile
 ** Ne fait rien si la pile d'origine est vide
 */
-void	push_a(t_stack *a, t_stack *b);
-void	push_b(t_stack *a, t_stack *b);
+void	push_a(t_stack *a, t_stack *b, int print);
+void	push_b(t_stack *a, t_stack *b, int print);
 
 /*
 ** --- ROTATE OPERATIONS ---
 ** Décale d'une position vers le haut tous les éléments de la pile
 ** Le premier élément devient le dernier
 */
-void	rotate_a(t_stack *a);
-void	rotate_b(t_stack *b);
-void	rotate_r(t_stack *a, t_stack *b);
+void	rotate_a(t_stack *a, int print);
+void	rotate_b(t_stack *b, int print);
+void	rotate_r(t_stack *a, t_stack *b, int print);
 
 /*
 ** --- REVERSE ROTATE OPERATIONS ---
 ** Décale d'une position vers le bas tous les éléments de la pile
 ** Le dernier élément devient le premier
 */
-void	rotate_rev_a(t_stack *a);
-void	rotate_rev_b(t_stack *b);
-void	rotate_rev_r(t_stack *a, t_stack *b);
+void	rotate_rev_a(t_stack *a, int print);
+void	rotate_rev_b(t_stack *b, int print);
+void	rotate_rev_r(t_stack *a, t_stack *b, int print);
 
 /*
 ** --- UTILS ---
